ba1f6f3ebdc849f253c20a097f375efc7b740dbb
acenosekai <acenosekai@gmail.com>
2015-01-06 22:12:52 +0700

    learn statechange event

--- assets/app/js/app.js
+++ assets/app/js/app.js
@@ -1,1 +1,14 @@
-var app = angular.module('app', ['ui.router']);
+var app = angular.module('app', ['ui.router'])
+    .run(function ($rootScope, $state) {
+        $rootScope.$on("$stateChangeStart", function (event, toState, toParams, fromState, fromParams) {
+            console.log(fromState);
+            console.log(toState);
+            
+
+//            if (toState.authenticate && !AuthService.isAuthenticated()) {
+//                // User isn’t authenticated
+//                $state.transitionTo("login");
+//                event.preventDefault();
+//            }
+        });
+    });
